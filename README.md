483040ee9eac8c3dfefc13d0297e0fddf58b368b
yuking <y.saito16@gmail.com>
2015-08-07 12:43:14 +0900

    gulpfile.js、package.jsonを追加

--- README.md
+++ README.md
@@ -1,30 +1,34 @@
 # gulp-sass-haml
-gulpでsassとhamlを使用するhtml5/css3テンプレート
+gulpでsass（compass-spriteのみ）とhamlを使用するhtml5テンプレート
 
-## package
+## package.json
 
-- browser-sync: ^2.7.7,
-- gulp: ^3.9.0,
-- gulp-concat: ^2.5.2,
-- gulp-cssmin: ^0.1.7,
-- gulp-plumber: ^1.0.1,
-- gulp-rename: ^1.2.2,
-- gulp-ruby-haml: 0.0.4,
-- gulp-sass: ^2.0.4,
-- gulp-uglify: ^1.2.0
+- browser-sync: "^2.7.7"
+- gulp: "^3.9.0"
+- gulp-autoprefixer: "^2.3.1"
+- gulp-compass: "^2.1.0"
+- gulp-concat: "^2.5.2"
+- gulp-cssmin: "^0.1.7"
+- gulp-plumber: "^1.0.1"
+- gulp-rename: "^1.2.2"
+- gulp-ruby-haml: "0.0.4"
+- gulp-uglify: "^1.2.0"
 
-## sass
+## sass（compass-sprite）
 
-- gulp-sass
+- gulp-compass
+
+rubyおよびsass、compassをインストールしてください。
 
 ## haml
 
 - gulp-ruby-haml
 - padrino-helpers
 
-rubyおよび各gemをインストールしてください。
+rubyおよびhaml、padrinoをインストールしてください。
 
 ## history
 
+- 2015/08/07 - gulpfile.js、package.jsonを追加
 - 2015/07/31 - update README
 - 2015/07/29 - create Repository
---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp');
+var haml = require('gulp-ruby-haml');
+var compass = require('gulp-compass');
+var autoprefixer = require("gulp-autoprefixer");
+var cssmin = require('gulp-cssmin');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var public = './public_html';
+
+gulp.task('default', ['browser-sync', 'watch']);
+
+// Watch
+gulp.task('watch', function() {
+  //gulp.watch('./_sass/**/*.scss', ['compass']);
+  gulp.watch('./_sass/**/*.scss', function(event) { gulp.run('compass'); });
+  gulp.watch(public + '/assets/javascripts/*.js', ['scripts']);
+  gulp.watch(public + '/_haml/**/*.haml', ['haml']);
+});
+
+// Script
+gulp.task('scripts', function() {
+  gulp.src(public + '/assets/javascripts/application.js')
+  //gulp.src(public + '/assets/javascripts/**/*.js')
+    //.pipe(concat('application.js'))
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(public + '/assets/javascripts'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// compass
+gulp.task('compass', function(){
+  gulp.src('_sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(compass({
+      config_file: 'config.rb',
+      comments: false,
+      css: public + '/assets/stylesheets/',
+      sass: '_sass/'
+    }))
+    .on('error', function(error) {
+      // Would like to catch the error here
+      console.log(error);
+      this.emit('end');
+    })
+    //.pipe(filter('**/*.css'))// cssのみを取り出す
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions', 'android >= 2.1']
+    }))
+    .pipe(gulp.dest(public + '/assets/stylesheets'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// css-min
+gulp.task('cssmin', function () {
+  gulp.src(public + '/assets/stylesheets/application.css')
+  .pipe(cssmin())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(gulp.dest(public + '/assets/stylesheets'));
+});
+
+// haml2html
+gulp.task('haml', function() {
+  gulp.src('/_haml/**/*.html.haml')
+    .pipe(plumber())
+    .pipe(haml({require: ['/_helpers/helpers.rb']}))
+    .pipe(gulp.dest(public))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// BrowserSync
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    proxy: 'localhost:80'// 環境にあわせて変更
+  });
+});
